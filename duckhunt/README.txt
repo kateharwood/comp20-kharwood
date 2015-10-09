0cc382cb1878ad0f7c64c4248ebb2415f99884cd
Kate Harwood <kateharwood>
2015-10-09 18:04:55 -0400

    finished lab5

---
+++ duckhunt/README.txt
@@ -0,0 +1,10 @@
+README for duckhunt Lab 5
+
+1. I believe that I have implemented all aspects of the lab correctly. I did not modify the HTML, CSS or image files, I did not use jQuery, and the images load onto the canvas when the page loads.
+
+2. I did not collaborate with anyone on this lab.
+
+3. I spent about 30 minutes on this lab.
+
+Sources:
+https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,18 @@
-// Replace this with your code...
+
+function init(){
+
+	var canvas = document.getElementById('game_canvas');
+	var ctx = canvas.getContext('2d');
+
+	var img = new Image();
+	img.src = "duckhunt-background.gif";
+
+	var duck = new Image();
+	duck.src = "duckhunt_various_sheet.png";
+
+	img.onload = function(){
+		ctx.drawImage(img, 0, 0);
+		ctx.drawImage(duck, 125, 115, 40, 30, 150, 40, 40, 30);
+		ctx.drawImage(duck, 35, 110, 40, 40, 75, 60, 40, 30);
+	}
+}
