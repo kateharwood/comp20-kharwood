95604a5c914af726d90cd10e1e94ce13e3b0aa50
Kate Harwood <kateharwood>
2015-10-20 21:12:23 -0400

    update lab 6

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,22 @@
-// Your JavaScript goes here...
+function parse() {
+
+	var request = new XMLHttpRequest();
+	var url = "data.json";
+
+	request.open("GET", url, true);
+
+	request.onreadystatechange = function(){
+		if(request.readyState == 4 && request.status == 200){
+			var response = JSON.parse(request.responseText);
+
+
+			elem = document.getElementById("messages");
+			for(key in response){
+				var message = response[key];
+				elem.innerHTML += "<div id='message'>" + message["content"] + "</div>" + " -" + "<div id='username'>" + message["username"] + "</div>";
+			}
+		}
+	}
+
+	request.send();
+}
